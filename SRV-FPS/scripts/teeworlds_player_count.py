2fb8855abc7249988da829c42327539ebded9245
BUISSON-DEBON Quentin <contact@qbuissondebon.info>
2016-07-06 22:42:55 +0200

    Fix some tabs instead of spaces (Thanks to my linter)

--- SRV-FPS/scripts/teeworlds_player_count.py
+++ SRV-FPS/scripts/teeworlds_player_count.py
@@ -11,18 +11,17 @@ TIMEOUT = 0.1
 REQUEST = b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xffgie3\xff"
 
 try:
-	address = sys.argv[1], int(sys.argv[2])
+    address = sys.argv[1], int(sys.argv[2])
 except (IndexError, ValueError):
-	print("usage: {} HOST PORT".format(sys.argv[0]), file=sys.stderr)
-	sys.exit(-1)
+    print("usage: {} HOST PORT".format(sys.argv[0]), file=sys.stderr)
+    sys.exit(-1)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.sendto(REQUEST, address)
 r,_,_ = select.select([s], [], [], TIMEOUT)
 if not r:
-	sys.exit(-2)
+    sys.exit(-2)
 
 data = s.recv(4096)
 data = data.split(b"\x00")
 print(data[6].decode())
-
