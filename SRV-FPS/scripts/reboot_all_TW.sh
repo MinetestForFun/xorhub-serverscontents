8d591041bc484b767746605df31ca5aaf9275f9a
BUISSON-DEBON Quentin <contact@qbuissondebon.info>
2016-07-19 00:21:50 +0200

    Twice speed for TW reboot script & add some comments for readability

--- SRV-FPS/scripts/reboot_all_TW.sh
+++ SRV-FPS/scripts/reboot_all_TW.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+# Reboot TeeWorlds Vanilla CTF
 screen -S "TeeWorlds_Vanilla_CTF" -X quit
-sleep 1
+sleep 0.5
 screen -dmS "TeeWorlds_Vanilla_CTF" /home/quentinbd/TeeWorlds_Vanilla_CTF/teeworlds_srv_d -f /home/quentinbd/TeeWorlds_Vanilla_CTF/serverconfig.cfg
-sleep 1
+sleep 0.5
+
+# Reboot TeeWorlds Vanilla DM
 screen -S "TeeWorlds_Vanilla_DM" -X quit
-sleep 1
+sleep 0.5
 screen -dmS "TeeWorlds_Vanilla_DM" /home/quentinbd/TeeWorlds_Vanilla_DM/teeworlds_srv_d -f /home/quentinbd/TeeWorlds_Vanilla_DM/serverconfig.cfg
-sleep 1
+sleep 0.5
+
+# Reboot TeeWorlds Vanilla DM
 screen -S "TeeWorlds_Vanilla_TDM" -X quit
-sleep 1
+sleep 0.5
 screen -dmS "TeeWorlds_Vanilla_TDM" /home/quentinbd/TeeWorlds_Vanilla_TDM/teeworlds_srv_d -f /home/quentinbd/TeeWorlds_Vanilla_TDM/serverconfig.cfg
-sleep 1
+sleep 0.5
+
+# Reboot TeeWorlds infclass
 screen -S "TeeWorlds_infclass" -X quit
-sleep 1
+sleep 0.5
 screen -dmS "TeeWorlds_infclass" /home/quentinbd/TeeWorlds_infclass/teeworlds_srv_d -f /home/quentinbd/TeeWorlds_infclass/autoexec.cfg
-sleep 1
+sleep 0.5
+
+# Reboot TeeWorlds OpenFNG
 screen -S "TeeWorlds_OpenFNG" -X quit
-sleep 1
+sleep 0.5
 screen -dmS "TeeWorlds_OpenFNG" /home/quentinbd/TeeWorlds_OpenFNG/openfng_srv -f /home/quentinbd/TeeWorlds_OpenFNG/openfng.cfg
-sleep 1
-echo ">>> Reboot done. <<<"
+sleep 0.5
+
+echo ">>> Reboot(s) done. <<<"
