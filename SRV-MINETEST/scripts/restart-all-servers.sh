c9cfc8ce08591fb43d3ab377cb6105466ea595d1
BUISSON-DEBON Quentin <contact@qbuissondebon.info>
2016-08-03 23:59:33 +0200

    Fix nice server and nice(low priority) mtwebmapper processes

--- SRV-MINETEST/scripts/restart-all-servers.sh
+++ SRV-MINETEST/scripts/restart-all-servers.sh
@@ -32,7 +32,7 @@ screen -dmS "mff-classic-mtredisalize" /home/quentinbd/gopath/bin/mtredisalize -
 sleep 1
 screen -dmS "mff-classic-mtwebmapper" /home/quentinbd/gopath/bin/mtwebmapper -colors=/home/quentinbd/mff/worlds/minetestforfun/colors.txt -web-host="" -web-port=8808 -map=/home/quentinbd/mff/worlds/minetestforfun/map.db -web=/var/www/mtsatellite -redis-host=localhost -redis-port=6379 -workers=1 -transparent=true -websockets=true -players=/home/quentinbd/mff/worlds/minetestforfun/mt_players_fifo
 sleep 1
-screen -dmS "mff-classic" nice -n10 /home/quentinbd/scripts/start-mff.sh
+screen -dmS "mff-classic" /home/quentinbd/scripts/start-mff.sh
 sleep 1
 screen -dmS "mff-hg-automappercolors" /home/quentinbd/mff-hg/games/minetestforfun_hg/mods/automappercolors/process.py /home/quentinbd/mff-hg/worlds/minetestforfun-hg /home/quentinbd/mff-hg/
 sleep 1
@@ -40,7 +40,7 @@ screen -dmS "mff-hg-mtredisalize" /home/quentinbd/gopath/bin/mtredisalize -host=
 sleep 1
 screen -dmS "mff-hg-mtwebmapper" /home/quentinbd/gopath/bin/mtwebmapper -colors=/home/quentinbd/mff-hg/worlds/minetestforfun-hg/colors.txt -web-host="" -web-port=8809 -map=/home/quentinbd/mff-hg/worlds/minetestforfun-hg/map.db -web=/var/www/mtsatellite-hg -redis-host=localhost -redis-port=6380 -workers=1 -transparent=true -websockets=true -players=/home/quentinbd/mff-hg/worlds/minetestforfun-hg/mt_players_fifo
 sleep 1
-screen -dmS "mff-hg" nice -n10 /home/quentinbd/scripts/start-mff-hg.sh
+screen -dmS "mff-hg" /home/quentinbd/scripts/start-mff-hg.sh
 sleep 1
 screen -dmS "mff-skyblock-automappercolors" /home/quentinbd/mff-skyblock/games/minetestforfun_skyblock/mods/automappercolors/process.py /home/quentinbd/mff-skyblock/worlds/minetestforfun-skyblock /home/quentinbd/mff-skyblock/
 sleep 1
@@ -48,7 +48,7 @@ screen -dmS mff-skyblock-mtredisalize /home/quentinbd/gopath/bin/mtredisalize -h
 sleep 1
 screen -dmS mff-skyblock-mtwebmapper /home/quentinbd/gopath/bin/mtwebmapper -colors=/home/quentinbd/mff-skyblock/worlds/minetestforfun-skyblock/colors.txt -web-host="" -web-port=8811 -map=/home/quentinbd/mff-skyblock/worlds/minetestforfun-skyblock/map.db -web=/var/www/mtsatellite-skyblock -redis-host=localhost -redis-port=6382 -workers=1 -transparent=true -websockets=true -players=/home/quentinbd/mff-skyblock/worlds/minetestforfun-skyblock/mt_players_fifo
 sleep 1
-screen -dmS "mff-skyblock" nice -n10 /home/quentinbd/scripts/start-mff-skyblock.sh
+screen -dmS "mff-skyblock" /home/quentinbd/scripts/start-mff-skyblock.sh
 sleep 1
 screen -dmS "mff-creative-automappercolors" /home/quentinbd/mff-creative/games/minetestforfun_creative/mods/automappercolors/process.py /home/quentinbd/mff-creative/worlds/minetestforfun-creative /home/quentinbd/mff-creative/
 sleep 1
@@ -56,7 +56,7 @@ screen -dmS mff-creative-mtredisalize /home/quentinbd/gopath/bin/mtredisalize -h
 sleep 1
 screen -dmS mff-creative-mtwebmapper /home/quentinbd/gopath/bin/mtwebmapper -colors=/home/quentinbd/mff-creative/worlds/minetestforfun-creative/colors.txt -web-host="" -web-port=8810 -map=/home/quentinbd/mff-creative/worlds/minetestforfun-creative/map.db -web=/var/www/mtsatellite-creative -redis-host=localhost -redis-port=6381 -workers=1 -transparent=true -websockets=true -players=/home/quentinbd/mff-creative/worlds/minetestforfun-creative/mt_players_fifo
 sleep 1
-screen -dmS "mff-creative" nice -n10 /home/quentinbd/scripts/start-mff-creative.sh
+screen -dmS "mff-creative" /home/quentinbd/scripts/start-mff-creative.sh
 sleep 1
 echo ">>> Démarrage de tous les serveurs - OK <<<"
 echo ">>> Tous les serveurs ont bien été REDEMARRÉ <<<"
