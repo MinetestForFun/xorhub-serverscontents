0985fbf92d8e04f6a7191f69a29e7785158236db
Darcidride <Darcidride@users.noreply.github.com>
2016-08-12 11:48:42 +0200

    Update update-all-servers.sh
    Let automappercolors.py the time to done the colors.txt file
    Linked to MinetestForFun/server-minetestforfun#503 (comment)

--- SRV-MINETEST/scripts/update-all-servers.sh
+++ SRV-MINETEST/scripts/update-all-servers.sh
@@ -39,7 +39,7 @@ echo ">>> UPDATE de tous les serveurs - OK <<<"
 echo ">>> Démarrage de tous les serveurs - En cours... <<<"
 ### Classic ###
     screen -dmS "mff-classic-automappercolors" /home/quentinbd/mff/mods/automappercolors/process.py /home/quentinbd/mff/worlds/minetestforfun/ /home/quentinbd/mff/
-    sleep 0.5
+    sleep 30
     screen -dmS "mff-classic-mtredisalize" /home/quentinbd/gopath/bin/mtredisalize -host=localhost -port=6379 -interleaved=true -change-url=http://localhost:8808/update -change-duration=15s /home/quentinbd/mff/worlds/minetestforfun/map.db
     sleep 0.5
     screen -dmS "mff-classic-mtwebmapper" nice -n 10 /home/quentinbd/gopath/bin/mtwebmapper -colors=/home/quentinbd/mff/worlds/minetestforfun/colors.txt -web-host="" -web-port=8808 -map=/home/quentinbd/mff/worlds/minetestforfun/map.db -web=/var/www/mtsatellite -redis-host=localhost -redis-port=6379 -workers=1 -transparent=true -websockets=true -players=/home/quentinbd/mff/worlds/minetestforfun/mt_players_fifo ulimit -n 4096
@@ -49,7 +49,7 @@ echo ">>> Démarrage de tous les serveurs - En cours... <<<"
 ### Hunger Games ###
     echo "Démarrage du serveur MinetestForFun Hunger Games en cours..."
     screen -dmS "mff-hg-automappercolors" /home/quentinbd/mff-hg/games/minetestforfun_hg/mods/automappercolors/process.py /home/quentinbd/mff-hg/worlds/minetestforfun-hg /home/quentinbd/mff-hg/
-    sleep 0.5
+    sleep 30
     screen -dmS "mff-hg-mtredisalize" /home/quentinbd/gopath/bin/mtredisalize -host=localhost -port=6380 -interleaved=true -change-url=http://localhost:8809/update -change-duration=15s /home/quentinbd/mff-hg/worlds/minetestforfun-hg/map.db
     sleep 0.5
     screen -dmS "mff-hg-mtwebmapper" nice -n 10 /home/quentinbd/gopath/bin/mtwebmapper -colors=/home/quentinbd/mff-hg/worlds/minetestforfun-hg/colors.txt -web-host="" -web-port=8809 -map=/home/quentinbd/mff-hg/worlds/minetestforfun-hg/map.db -web=/var/www/mtsatellite-hg -redis-host=localhost -redis-port=6380 -workers=1 -transparent=true -websockets=true -players=/home/quentinbd/mff-hg/worlds/minetestforfun-hg/mt_players_fifo ulimit -n 4096
@@ -58,7 +58,7 @@ echo ">>> Démarrage de tous les serveurs - En cours... <<<"
     sleep 0.5
 ### SkyBlock ###
     screen -dmS "mff-skyblock-automappercolors" /home/quentinbd/mff-skyblock/games/minetestforfun_skyblock/mods/automappercolors/process.py /home/quentinbd/mff-skyblock/worlds/minetestforfun-skyblock /home/quentinbd/mff-skyblock/
-    sleep 0.5
+    sleep 30
     screen -dmS "mff-skyblock-mtredisalize" /home/quentinbd/gopath/bin/mtredisalize -host=localhost -port=6382 -interleaved=true -change-url=http://localhost:8811/update -change-duration=15s /home/quentinbd/mff-skyblock/worlds/minetestforfun-skyblock/map.db
     sleep 0.5
     screen -dmS "mff-skyblock-mtwebmapper" nice -n 10 /home/quentinbd/gopath/bin/mtwebmapper -colors=/home/quentinbd/mff-skyblock/worlds/minetestforfun-skyblock/colors.txt -web-host="" -web-port=8811 -map=/home/quentinbd/mff-skyblock/worlds/minetestforfun-skyblock/map.db -web=/var/www/mtsatellite-skyblock -redis-host=localhost -redis-port=6382 -workers=1 -transparent=true -websockets=true -players=/home/quentinbd/mff-skyblock/worlds/minetestforfun-skyblock/mt_players_fifo ulimit -n 4096
@@ -67,7 +67,7 @@ echo ">>> Démarrage de tous les serveurs - En cours... <<<"
     sleep 0.5
 ### Creative ###
     screen -dmS "mff-creative-automappercolors" /home/quentinbd/mff-creative/games/minetestforfun_creative/mods/automappercolors/process.py /home/quentinbd/mff-creative/worlds/minetestforfun-creative /home/quentinbd/mff-creative/
-    sleep 0.5
+    sleep 30
     screen -dmS "mff-creative-mtredisalize" /home/quentinbd/gopath/bin/mtredisalize -host=localhost -port=6381 -interleaved=true -change-url=http://localhost:8810/update -change-duration=15s /home/quentinbd/mff-creative/worlds/minetestforfun-creative/map.db
     sleep 0.5
     screen -dmS "mff-creative-mtwebmapper" nice -n 10 /home/quentinbd/gopath/bin/mtwebmapper -colors=/home/quentinbd/mff-creative/worlds/minetestforfun-creative/colors.txt -web-host="" -web-port=8810 -map=/home/quentinbd/mff-creative/worlds/minetestforfun-creative/map.db -web=/var/www/mtsatellite-creative -redis-host=localhost -redis-port=6381 -workers=1 -transparent=true -websockets=true -players=/home/quentinbd/mff-creative/worlds/minetestforfun-creative/mt_players_fifo ulimit -n 4096
